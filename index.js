efc9b5dcb3ec1c158d4685f41ff016113363fa4b
Joanna <40646853+Joabulla07@users.noreply.github.com>
2025-09-24 13:28:51 -0300

    cors

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 // Add CORS configuration
 app.use(cors({
-    origin: [*], //agregar el deploy del front
+    origin: ['*'], //agregar el deploy del front
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
