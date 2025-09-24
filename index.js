c0cf9f3ad053f17fc15fa8dca24d01366714cab8
Joanna <40646853+Joabulla07@users.noreply.github.com>
2025-09-24 13:27:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 // Add CORS configuration
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'], //agregar el deploy del front
+    origin: [*], //agregar el deploy del front
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
