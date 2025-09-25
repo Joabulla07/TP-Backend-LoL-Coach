338d7578eb0f33aca8575cb60f64ce1ff60a8224
Joanna <40646853+Joabulla07@users.noreply.github.com>
2025-09-25 17:28:15 -0300

    chat route

--- src/routers/chatRouter.js
+++ src/routers/chatRouter.js
@@ -5,6 +5,6 @@ import {chat, resetChat} from "../controllers/chatController.js";
 export const chatRoute = express.Router();
 
 chatRoute.post('/chat', verifyTokenMiddleware, chat)
-chatRoute.post('/chat/reset', verifyTokenMiddleware, resetChat)
+chatRoute.post('/reset', verifyTokenMiddleware, resetChat)
 
 
