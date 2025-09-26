fb321ba499efa4a3b2d45a382326c3d929371c5b
Joanna <40646853+Joabulla07@users.noreply.github.com>
2025-09-26 15:51:26 -0300

    fix url

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 // Configuración de CORS para permitir cualquier origen
 app.use(cors({
-    origin: 'http://localhost:5173',  // si no tira error de cors, hay que definir de a uno por las cookies
+    origin: ['http://localhost:5173', 'http://league-of-coaching.netlify.app/'],  // si no tira error de cors, hay que definir de a uno por las cookies
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Si necesitas soportar credenciales
